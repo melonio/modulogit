b6ff34cbe8486f6b15e942890aa155159cd1d2b9
Daniel C. Melonio <danielcmelonio@outlook.com>
2018-05-27 12:32:03 -0300

    Modificações feitas à sugestão do Fábio

--- main.cs
+++ main.cs
@@ -3,4 +3,5 @@ static void Main(string[] args)
 	int a = 25;
 	int b = 30;
 	print.out.ln(a + b);
+	System.Console.readline("Olá, mundo!");
 }
--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main()
 	static void Main()
 	{
 		print.out.ln("Let's go!");
+		print.out.ln("Vamos lá, ver se funciona!");
 	}
 }
